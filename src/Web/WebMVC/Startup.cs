4e32391ca6803423169481ce0e1eb9470006cd09
Zhiming Lin <zhiming.lin@quest.com>
2021-04-13 15:59:43 +0800

    Fixed IDS4 issues.

--- src/Web/WebMVC/Startup.cs
+++ src/Web/WebMVC/Startup.cs
@@ -75,7 +75,7 @@ namespace Microsoft.eShopOnContainers.WebMVC
 
             // Fix samesite issue when running eShop from docker-compose locally as by default http protocol is being used
             // Refer to https://github.com/dotnet-architecture/eShopOnContainers/issues/1391
-            app.UseCookiePolicy(new CookiePolicyOptions { MinimumSameSitePolicy = AspNetCore.Http.SameSiteMode.Lax });
+            app.UseCookiePolicy(new CookiePolicyOptions { CheckConsentNeeded = context => true, MinimumSameSitePolicy = AspNetCore.Http.SameSiteMode.Lax });
 
             app.UseRouting();
 
